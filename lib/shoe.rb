26dd13291d43a113f982fb949fd4facfa85d1acc
Will Hodgson <will.hodgson@current-rms.com>
2019-08-01 12:02:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,13 +1,1 @@
 # Make your shoe class here!
-class Book
-  def initialize(title)
-    @title = title
-  end
-
-  attr_reader :title
-  attr_accessor :author, :page_count, :genre
-
-  def turn_page
-    @page_count += 1
-  end
-end
